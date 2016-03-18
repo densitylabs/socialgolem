f37ba0c7551f733c9c815bdc1fe2830c24d18dca
Tonatiuh Nunez <tonatiuhnb@gmail.com>
2016-03-18 12:12:26 -0600

    add missing home controller tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,22 +5,15 @@ def login_user(user = nil)
   session[:user_id] = user.id
 end
 
-RSpec.describe HomeController, 'GET#index' do
-  # context 'if users is not logged in' do
-  #   it 'redirects to Twitter' do
-  #   end
-  # end
-  #
-  # context 'if user is in the middle ' do
-  # end
+describe HomeController, 'GET#index' do
   it 'responds successfully' do
     login_user
     get :index
-    expect(response).to have_http_status(:ok)
+    expect(response).to have_http_status :ok
   end
 end
 
-RSpec.describe HomeController, 'GET#unfriendly_users' do
+describe HomeController, 'GET#unfriendly_users' do
   let(:unfriendly_users) { double('unfriendly_users') }
 
   before do
@@ -30,17 +23,13 @@ RSpec.describe HomeController, 'GET#unfriendly_users' do
 
   it 'responds successfully' do
     # check before
-
     get :unfriendly_users
-
-    expect(response).to have_http_status(:ok)
+    expect(response).to have_http_status :ok
   end
 
   it 'assigns users' do
     # check before
-
     get :unfriendly_users
-
     expect(assigns(:users)).to eq unfriendly_users
   end
 end
@@ -56,25 +45,69 @@ describe HomeController, 'POST #unfollow_users' do
 
   it 'unfollows the pointed users' do
     # check before
-
     post :unfollow_users, users_ids: '1,2'
-
     expect(connector).to have_received(:unfollow_users).with(%w(1 2))
   end
 
   it 'responds successfully' do
     # check before
-
     post :unfollow_users, users_ids: '1,2'
-
-    expect(response).to have_http_status(:ok)
+    expect(response).to have_http_status :ok
   end
 
   it 'assigns users' do
-    #check before
-
+    # check before
     post :unfollow_users, users_ids: '1,2'
+    expect(assigns(:users)).to eq(twitter_response)
+  end
+end
 
+describe HomeController, 'GET #users_im_unfriendly_with' do
+  let(:users_im_unfriendly_with) { double('users_im_unfriendly_with') }
+
+  before do
+    login_user
+    TwitterUserConnector.any_instance.stub(
+      users_im_unfriendly_with: users_im_unfriendly_with)
+  end
+
+  it 'responds successfully' do
+    # check before
+    get :users_im_unfriendly_with
+    expect(response).to have_http_status :ok
+  end
+
+  it 'assigns users' do
+    # check before
+    get :users_im_unfriendly_with
+    expect(assigns(:users)).to eq users_im_unfriendly_with
+  end
+end
+
+describe HomeController, 'POST #follow_users', focus: true do
+  let(:twitter_response) { [{}, {}] }
+  let(:connector) { double(follow_users: twitter_response) }
+
+  before do
+    login_user
+    TwitterUserConnector.stub(new: connector)
+  end
+
+  it 'follows the pointed users' do
+    # check before
+    post :follow_users, users_ids: '1,2'
+    expect(connector).to have_received(:follow_users).with(%w(1 2))
+  end
+
+  it 'responds successfully' do
+    # check before
+    post :follow_users, users_ids: '1,2'
+    expect(response).to have_http_status :ok
+  end
+
+  it 'assigns users' do
+    # check before
+    post :follow_users, users_ids: '1,2'
     expect(assigns(:users)).to eq(twitter_response)
   end
 end
