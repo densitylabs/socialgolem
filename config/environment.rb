38d651f101418d2d1b65329f388edc546bff1b5c
Tonatiuh Nunez <tonatiuhnb@gmail.com>
2016-03-22 18:33:35 -0600

    update config files for rails 5

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-TwitterBot::Application.initialize!
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TwitterBot::Application.config.session_store :cookie_store, key: '_twitter_client_session'
+Rails.application.config.session_store :cookie_store, key: '_twitter_bot_session'
